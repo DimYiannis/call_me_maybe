dcd65ec96ff63a8e06ccb8bf492c364c3d10bedf
yiannis <107484245+DimYiannis@users.noreply.github.com>
2026-06-24 16:43:34 +0200

    model name

--- src/__main__.py
+++ src/__main__.py
@@ -44,7 +44,8 @@ def main() -> None:
     functions = load_functions(args.functions_definition)
     prompts = load_prompts(args.input)
 
-    model = Small_LLM_Model(model_name="Qwen/Qwen3-1.7B") #model_name="Qwen/Qwen3-1.7B" Qwen/Qwen2.5-1.5B-Instruct
+    model = Small_LLM_Model() 
+    #model_name="Qwen/Qwen3-1.7B" Qwen/Qwen2.5-1.5B-Instruct
     vocab = Vocabulary(model)
 
     results = []
